36f00c97efa634ed3fc1664e6ec9d9e550aa1bd7
reiki <Zero4one@ya.ru>
2023-12-28 15:23:25 +0500

    Создал файл тем

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'instrument_page/instrument_page.dart';
 import 'package:lotosui/navigate_page.dart';
 import 'package:flutter/material.dart';
 
+import 'themes.dart';
+
 /*
 Сделать чтобы при нажатии на инструмент (актив) была страница его
 и кнопка включить и он переходил в активные
@@ -58,36 +60,6 @@ class AutoTraderApp extends StatelessWidget {
         '/instrumentInfo': (context) => const InstrumentPage(),
       },
       home: const NavigatePage(),
-    );
-  }
-
-  // Темы
-  ThemeData appTheme(BuildContext context, brightness) {
-    return ThemeData(
-      colorScheme: ColorScheme.fromSeed(
-        brightness: brightness,
-        seedColor: Colors.pink,
-      ),
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        elevation: 0,
-        titleTextStyle: TextStyle(
-          color: brightness == Brightness.light ? Colors.black : Colors.white,
-          fontSize: 22,
-          fontWeight: FontWeight.w500,
-        ),
-      ),
-      textTheme: const TextTheme(
-        bodyMedium: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
-        ),
-        labelSmall: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w200,
-        ),
-      ),
     );
   }
 }
